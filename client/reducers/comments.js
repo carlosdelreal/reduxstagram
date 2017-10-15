ae2dd84346d2631815524feb6b430d0d0c62216f
carlosdelreal <csdelreal@gmail.com>
2017-10-15 15:39:21 -0500

    understanding the reducer's job and dispatching actions

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,3 +1,13 @@
+// The reducer does the job of maintaining state.
+// Every time that you run (displatch) an action, every single reducer will run
+// and wether you choose to act on that action or not is up to the reducer.
+// In other words, whenever something happens, all reducers run and changes in state are up to you.
+
+// a reducer takes in two things:
+
+// 1. the action (info about what happened)
+// 2. copy of current state
+
 function comments(state = [], action) {
 	console.log(state, action);
 	return state;
--- client/reducers/posts.js
+++ client/reducers/posts.js
@@ -1,9 +1,15 @@
+// The reducer does the job of maintaining state.
+// Every time that you run (displatch) an action, every single reducer will run
+// and wether you choose to act on that action or not is up to the reducer.
+// In other words, whenever something happens, all reducers run and changes in state are up to you.
+
 // a reducer takes in two things:
 
 // 1. the action (info about what happened)
 // 2. copy of current state
 
 function posts(state = [], action) {
+	console.log('The post will change!');
 	console.log(state, action);
 	return state;
 }
